d4a097c562038b74dc3724fffb542e51b903a836
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-04-01 20:43:46 +0300

    Changed reset_user_votes

--- core/management/commands/reset_user_votes.py
+++ core/management/commands/reset_user_votes.py
@@ -1,12 +1,20 @@
+from datetime import timedelta
+
 from django.core.management import BaseCommand
+from django.utils import timezone
 
 from user.models import User
 
 
 def reset_user_votes():
-    User.objects.all().update(vote_count=5, first_vote=None)
+    twelve_hours_ago = timezone.now() - timedelta(hours=12)
 
-    for user in User.objects.all():
+    users_to_update = User.objects.filter(first_vote__lte=twelve_hours_ago)
+
+    for user in users_to_update:
+        user.vote_count = 5
+        user.first_vote = None
+        user.save()
         user.vote_coins.clear()
 
 
