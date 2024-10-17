442c33f3b0919c4b8951d22ee72772ac1246e5df
Mikhaylo Kisil <mihajlokisil20@gmail.com>
2024-10-17 16:42:59 +0300

    Updated block FAQ

--- static/js/faq.js
+++ static/js/faq.js
@@ -1,10 +1,22 @@
-const details = document.querySelectorAll("details");
-details.forEach((targetDetail) => {
-  targetDetail.addEventListener("click", () => {
-    details.forEach((detail) => {
-      if (detail !== targetDetail) {
-        detail.removeAttribute("open");
-      }
+document.addEventListener('DOMContentLoaded', function () {
+    const details = document.querySelectorAll("details");
+    details.forEach((targetDetail) => {
+        targetDetail.addEventListener("click", () => {
+            details.forEach((detail) => {
+                if (detail !== targetDetail) {
+                    detail.removeAttribute("open");
+                }
+            });
+        });
     });
-  });
+
+    document.querySelector('.faq-btn').addEventListener('click', function () {
+        const faqContent = document.querySelector('.d-display');
+        faqContent.classList.toggle('show');
+        if (faqContent.classList.contains('show')) {
+            setTimeout(() => {
+                faqContent.scrollIntoView({behavior: 'smooth'});
+            }, 90);
+        }
+    });
 });
